c58ddd5397b9ac15bf6ffeae29968b719831ce60
Cherloy <48054620+Cherloy@users.noreply.github.com>
2023-07-03 17:46:37 +0300

    KAL

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Читать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(191, 207);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Писать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Номер регистра";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Кол-во регистров";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(289, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(289, 98);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(5, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(429, 250);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Чтение";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Location = new System.Drawing.Point(462, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(426, 249);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Запись";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Значение регистра";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Номер регистра";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(275, 98);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(276, 29);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dataGridView1);
+            this.groupBox3.Controls.Add(this.groupBox4);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Location = new System.Drawing.Point(5, 283);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(883, 335);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Регистры";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(7, 19);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(406, 310);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Номер";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Значение";
+            this.Column2.Name = "Column2";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label7);
+            this.groupBox4.Controls.Add(this.button5);
+            this.groupBox4.Controls.Add(this.comboBox2);
+            this.groupBox4.Controls.Add(this.comboBox1);
+            this.groupBox4.Controls.Add(this.label5);
+            this.groupBox4.Controls.Add(this.button4);
+            this.groupBox4.Controls.Add(this.label6);
+            this.groupBox4.Location = new System.Drawing.Point(719, 62);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox4.Size = new System.Drawing.Size(146, 219);
+            this.groupBox4.TabIndex = 14;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Подключение";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Red;
+            this.label7.Location = new System.Drawing.Point(11, 123);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Нет соединения";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(13, 175);
+            this.button5.Margin = new System.Windows.Forms.Padding(2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(92, 19);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Отключиться";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "300",
+            "600",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "38400",
+            "56000",
+            "57600",
+            "115200",
+            "128000",
+            "153600",
+            "230400",
+            "256000",
+            "460800",
+            "921600"});
+            this.comboBox2.Location = new System.Drawing.Point(14, 82);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(92, 21);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(14, 33);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(92, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 18);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Порт";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(14, 151);
+            this.button4.Margin = new System.Windows.Forms.Padding(2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(92, 19);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Подключиться";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(11, 67);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Скорость";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(733, 19);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Очистить список";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 630);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using Modbus.Device;
+using Modbus;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        SerialPort sp;
+        ModbusSerialMaster master;
+        byte slaveID = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sp = new SerialPort(); //Create a new SerialPort object.
+            master = ModbusSerialMaster.CreateRtu(sp);
+
+            comboBox1.Text = "COM1";
+            comboBox2.Text = "9600";
+            sp.DataBits = 8;
+            sp.Parity = Parity.None;
+            sp.StopBits = StopBits.One;
+            master.Transport.ReadTimeout = 300;
+            master.Transport.WriteTimeout = 300;
+
+            for(int i = 1; i <= 256; ++i)
+            {
+                comboBox1.Items.Add("COM" + i);
+            } 
+            button5.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Parse_RD;
+            ushort Reg_Nom_Rd = 0;
+            ushort Reg_Num_Rd = 0;
+
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                if (int.TryParse(textBox1.Text, out Parse_RD) && int.TryParse(textBox2.Text, out Parse_RD))
+                {
+                    Reg_Nom_Rd = Convert.ToUInt16(textBox1.Text);
+                    Reg_Num_Rd = Convert.ToUInt16(textBox2.Text);
+                    ModbusSerialMaster master = ModbusSerialMaster.CreateRtu(sp);
+
+                    try
+                    {
+                        try
+                        {
+                            dataGridView1.Rows.Clear();
+                        }
+                        catch { }
+
+                        ushort[] holding_register = master.ReadHoldingRegisters(slaveID, Reg_Nom_Rd, Reg_Num_Rd);
+                        for (int i = 0; i < holding_register.Length; ++i)
+                        {
+                            dataGridView1.Rows.Add(Reg_Nom_Rd + i,holding_register[i]);  
+                        }
+
+                    }
+                    catch (TimeoutException)
+                    {
+                        MessageBox.Show("Не обнаружено подчиненных устройств в выбранном порту\r\n Пожалуйста, выберите другой порт");
+                    }
+                    catch (SlaveException)
+                    {
+                        MessageBox.Show("Введено больше регистров, чем существует в подчиненном устройстве");
+                    }
+                    catch (ArgumentException)
+                    {
+                        MessageBox.Show("Введите ненулевое количество регистров");
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        MessageBox.Show("Вы не подключились к порту");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Введите целочисленное значение");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите корректные данные");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int Parse_WR;
+            ushort Reg_Nom_WR = 0;
+            ushort Reg_Val_WR = 0;
+
+            if (textBox3.Text != "" && textBox4.Text != "")
+            {
+                if (int.TryParse(textBox3.Text, out Parse_WR) && int.TryParse(textBox4.Text, out Parse_WR))
+                {
+                    Reg_Nom_WR = Convert.ToUInt16(textBox3.Text);
+                    Reg_Val_WR = Convert.ToUInt16(textBox4.Text);
+                    ModbusSerialMaster master = ModbusSerialMaster.CreateRtu(sp);
+
+                    try
+                    {
+                        dataGridView1.Rows[Reg_Nom_WR].SetValues(Reg_Nom_WR, Reg_Val_WR);
+                    }
+                    catch{}
+
+                    try
+                    {
+                        master.WriteSingleRegister(slaveID, Reg_Nom_WR, Reg_Val_WR);
+                    }
+                    catch (TimeoutException)
+                    {
+                        MessageBox.Show("Не обнаружено подчиненных устройств в выбранном порту\r\n Пожалуйста, выберите другой порт");
+                    }
+                    catch (SlaveException)
+                    {
+                        MessageBox.Show("Данного регистра не существует в подчиненном устройстве");
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        MessageBox.Show("Вы не подключились к порту");
+                    }
+                    
+                }
+                else {
+                MessageBox.Show("Введите одно целочисленное значение");  
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите данные");
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear(); 
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            dataGridView1.Rows.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                try
+                {
+                    ModbusSerialMaster master = ModbusSerialMaster.CreateRtu(sp);
+                    master.Dispose();
+                }
+                catch{}
+
+                sp.PortName = comboBox1.Text;
+                sp.BaudRate = Convert.ToUInt16(comboBox2.Text);
+                sp.Open();
+                label7.Text = "Есть соединение";
+                label7.ForeColor = Color.Green;
+                button5.Enabled = true;
+            }
+            catch
+            {
+                MessageBox.Show("Данный порт занят");
+                label7.Text = "Подключение не удалось";
+                label7.ForeColor = Color.Orange;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ModbusSerialMaster master = ModbusSerialMaster.CreateRtu(sp);
+                master.Dispose();
+                label7.Text = "Нет соединения";
+                label7.ForeColor = Color.Red;
+                button5.Enabled = false;
+            }
+            catch
+            {
+                MessageBox.Show("Вы не были подключены к порту");
+            }
+        }
+
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                master.Dispose();
+            }
+            catch {}
+        }
+    }
+}
